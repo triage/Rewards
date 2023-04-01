c5c9643dee1042af414270324b6caeb41297d263
Eric Schulte <triage@users.noreply.github.com>
2023-04-01 12:36:38 -0400

    cleanup

--- tests/unit/test_balance.py
+++ tests/unit/test_balance.py
@@ -37,14 +37,7 @@ class MockQLDBDriver(Driver):
 def lambda_context():
     class LambdaContext:
         def __init__(self):
-            self.function_name = "test-func"
-            self.memory_limit_in_mb = 128
-            self.invoked_function_arn = "arn:aws:lambda:eu-west-1:809313241234:function:test-func"
-            self.aws_request_id = "52fdfc07-2182-154f-163f-5f0f9a621d72"
-            os.environ["LEDGER_NAME"] = 'rewards-ledgerstore-test'
-
-        def get_remaining_time_in_millis(self) -> int:
-             return 1000
+            self.function_name = "test_balance"
 
     return LambdaContext()
 
