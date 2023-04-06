d0fdcc9cd0822144d014cdb009000ec8d2c9bcd7
Eric Schulte <triage@users.noreply.github.com>
2023-04-06 16:24:44 -0400

    paths to tests

--- tests/unit/__init__.py
+++ tests/unit/__init__.py
@@ -1,7 +1,3 @@
 import sys
-# sys.path.append('./src/')
-# sys.path.append('./src/balance')
-# sys.path.append('./src/issuer')
 sys.path.append('../../src/merchant')
-# sys.path.append('./src/merchant/redeem')
-sys.path.append('../../src/qldb_helper')
+sys.path.append('../../src/rewards_dao')
